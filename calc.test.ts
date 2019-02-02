a204245ad1a605b2edcafe54c2a17c7a179c0d8e
Инютин Максим Николаевич <inyutin_maxim@mail.ru>
2019-02-02 14:33:43 +0300

    calc tests

---
+++ calc.test.ts
@@ -0,0 +1,69 @@
+import { Calc } from './calc';
+
+describe('calc', () => {
+	beforeEach(() => {
+		this.calc = new Calc();
+	});
+	describe('sum method', () => {
+		test('calc.sum(1, 2) = 3', () => {
+			expect(this.calc.sum(1, 2)).toBe(3);
+		});
+		
+		test('calc.sum(5, 7) = 12', () => {
+			expect(this.calc.sum(5, 7)).toBe(12);
+		});
+		
+		test('calc.sum(-5, 7) = 2', () => {
+			expect(this.calc.sum(-5, 7)).toBe(2);
+		});
+		
+		test('calc.sum(5, -7) = -2', () => {
+			expect(this.calc.sum(5, -7)).toBe(-2);
+		});
+	});
+	
+	describe('minus method', () => {
+		test('calc.minus(1, 3) = -2', () => {
+			expect(this.calc.minus(1, 3)).toBe(-2);
+		});
+		test('calc.minus(5, 7) = -2', () => {
+			expect(this.calc.minus(5, 7)).toBe(-2);
+		});
+		test('calc.minus(-5, 7) = -12', () => {
+			expect(this.calc.minus(-5, 7)).toBe(-12);
+		});
+		test('calc.minus(5, -7) = 12', () => {
+			expect(this.calc.minus(5, -7)).toBe(12);
+		});
+	});
+	
+	describe('multiply method', () => {
+		test('calc.multiply(1, 3) = 3', () => {
+			expect(this.calc.multiply(1, 3)).toBe(3);
+		});
+		test('calc.multiply(5, 7) = 35', () => {
+			expect(this.calc.multiply(5, 7)).toBe(35);
+		});
+		test('calc.multiply(-5, 7) = -35', () => {
+			expect(this.calc.multiply(-5, 7)).toBe(-35);
+		});
+		test('calc.multiply(5, -7) = -35', () => {
+			expect(this.calc.multiply(5, -7)).toBe(-35);
+		});
+	});
+	
+	describe('division method', () => {
+		test('calc.division(6, 3) = 2', () => {
+			expect(this.calc.division(6, 3)).toBe(2);
+		});
+		test('calc.division(-9, 3) = -3', () => {
+			expect(this.calc.division(-9, 3)).toBe(-3);
+		});
+		test('calc.division(9, -3) = -3', () => {
+			expect(this.calc.division(9, -3)).toBe(-3);
+		});
+		test('calc.division(25, 5) = 5', () => {
+			expect(this.calc.division(25, 5)).toBe(5);
+		});
+	});
+});
---
+++ calc.ts
@@ -0,0 +1,17 @@
+export class Calc {
+	public sum(left: number, right: number): number {
+		return left + right;
+	}
+	
+	public minus(left: number, right: number): number {
+		return left - right;
+	}
+	
+	public multiply(left: number, right: number): number {
+		return left * right;
+	}
+	
+	public division(left: number, right: number): number {
+		return left / right;
+	}
+}
